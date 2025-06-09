4564477ed3572c3e70254d6ca46be5306f2c2e30
julioElpabo <christophe.roulet@edufr.ch>
2025-06-09 15:43:57 +0200

    Premier commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TissageView />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TissageView from './components/Tissage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TissageView
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap'; // (JS des composants comme modals, tooltips, etc.)
+
+
+
 
 Vue.config.productionTip = false
 
